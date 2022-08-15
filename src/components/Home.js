c4e9083294d1d956dfb3f8772a2519db1bc8d54a
Diana <arantxa.codes@gmail.com>
2022-08-15 13:57:45 -0700

    changed colors to b&w

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,39 +32,45 @@ console.log('home',props.makeupItems)
 //     };
 return (
 <div className="home-page">
-<section className="right-side">
     <h1>hello!</h1>
-        <section className="category-section">
-            <span className="category-header">MY ITEMS</span>
-            <section className="categories">
-                <Link to="products">
-                    <span className="category-name" onClick={getMakeup}>MAKEUP</span>
+    <section className="section-container">
+        <section className="right-side">
+                <section className="category-section">
+                    <span className="category-header">MY ITEMS</span>
+                    <section className="categories">
+                        {/*<section className="categories-top">*/}
+                        <Link to="products">
+                            <span className="category-name" onClick={getMakeup}>MAKEUP</span>
+                        </Link>
+                        <Link to="products">
+                            <span className="category-name" onClick={getSkincare}>SKINCARE</span>
+                        </Link>
+                        {/*</section>*/}
+                        {/*<section className="categories-bottom">*/}
+                        <Link to="products">
+                            <span className="category-name" onClick={getSubscriptions}>SUBSCRIPTIONS</span>
+                        </Link>
+                        <Link to="products">
+                            <span className="items category-name" onClick={getProductsFromApi}>ALL</span>
+                        </Link>
+                        {/*</section>*/}
+                    </section>
+                </section>
+                <Link to="new">
+                    {/*<span onClick={resetForm} className="new-section">ADD NEW PRODUCT</span>*/}
+                    <span className="new-section">ADD NEW PRODUCT</span>
                 </Link>
-                <Link to="products">
-                    <span className="category-name" onClick={getSkincare}>SKINCARE</span>
-                </Link>
-                <Link to="products">
-                    <span className="category-name" onClick={getSubscriptions}>SUBSCRIPTIONS</span>
-                </Link>
-                <Link to="products">
-                    <span className="items category-name" onClick={getProductsFromApi}>ALL</span>
-                </Link>
-            </section>
+
         </section>
-        <Link to="new">
-            {/*<span onClick={resetForm} className="new-section">ADD NEW PRODUCT</span>*/}
-            <span className="new-section">ADD NEW PRODUCT</span>
-        </Link>
+        <section className="left-side">
+            <section className="chart-section">
+                <CategoryPie makeup={props.makeupItems} skincare={props.skincareItems} subscriptions={props.subscriptionItems}/>
+                <UsePie all={props.sumCount} makeup={props.makeupCount} skincare={props.skincareCount} subscriptions={props.subscriptionsCount} />
+            </section>
+            <Countdown products={props.expiring} />
 
-</section>
-<section className="left-side">
-    <section className="chart-section">
-        <CategoryPie makeup={props.makeupItems} skincare={props.skincareItems} subscriptions={props.subscriptionItems}/>
-        <UsePie all={props.sumCount} makeup={props.makeupCount} skincare={props.skincareCount} subscriptions={props.subscriptionsCount} />
+        </section>
     </section>
-    <Countdown products={props.expiring} />
-
-</section>
 </div>
 );
 }
